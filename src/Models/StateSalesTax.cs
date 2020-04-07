eb051256da0e1d06a690181dd2428550d5b54713
jwthrs <jeweathe@uncg.edu>
2020-04-07 18:59:54 -0400

    Documented db models

--- src/Models/StateSalesTax.cs
+++ src/Models/StateSalesTax.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace SalesTaxCalculator.Models
 {
+    /// <summary>
+    /// StateSalesTax is an Entity Framework implementation of the SQL StateSalesTax table.
+    /// Each StateSalesTax has a name for the state and a state-wide tax-rate, and a list of contained counties with their own names and tax-rates.
+    /// </summary>
     public class StateSalesTax 
     {
         public int Id {get; set;}
@@ -13,6 +17,10 @@ namespace SalesTaxCalculator.Models
         public List<CountyTax> CountyTaxes { get; set; }
     }
 
+    /// <summary>
+    /// CountyTax is an Entity Framework implementation of the SQL CountyTax table.
+    /// Each CountyTax has a name for the county and a county-wide tax-rate.
+    /// </summary>
     public class CountyTax
     {
         public int Id {get; set;}
---
+++ test/SalesTaxCalculator.UnitTests/ControllerTests/SalesTaxControllerTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SalesTaxCalculator.Context;
+using SalesTaxCalculator.Services;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SalesTaxCalculator.Models;
+
+
+namespace SalesTaxCaulcator.UnitTests.ControllerTests
+{
+	[TestClass]
+	public class SalesTaxControllerTests
+	{
+
+		private Mock<SalesTaxContext> _mockContext;
+		
+		public SalesTaxControllerTests()
+		{
+			_mockContext = new Mock<SalesTaxContext>();
+		}
+
+		[TestMethod] 
+		public async Task TestValidRequest()
+		{
+			var stateTaxObj = new StateSalesTax
+			{
+				Id = 1,
+				Name = "NOBODY",
+				TaxRate = "0",
+				CountyTaxes = new List<CountyTax>
+				{
+					new CountyTax
+					{
+						Id = 1,
+						Name = "NoCounty",
+						TaxRate = "0"
+					}
+				}
+			}; 
+			
+			_mockContext.Setup(m => m.RetrieveState(stateTaxObj.Name)).ReturnsAsync(stateTaxObj);
+			var mediator = new SalesTaxMediator(_mockContext.Object);
+
+			var result = await mediator.CalculateSalesTaxAsync(new SalesTaxRequest
+			{
+				State = stateTaxObj.Name,
+				County = stateTaxObj.CountyTaxes.First().Name,
+				ItemPrice = "19.99"
+			});
+			
+			(result as ViewResult)?.Model.Should().BeEquivalentTo(new SalesTaxResponse
+			{
+				State = stateTaxObj.Name,
+				County = stateTaxObj.CountyTaxes.First().Name,
+				StateTax = 0,
+				LocalTax = 0,
+				TotalTax = 0
+			});
+			
+			// Throw exception if never called
+			_mockContext.Verify(m => m.RetrieveState(stateTaxObj.Name), Times.Once());
+		}
+	}
+}
